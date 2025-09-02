de90ac1854428a69a7f705b956e3bdee60409fb4
Levi <101576292+lnatamm@users.noreply.github.com>
2025-09-02 08:19:12 -0300

    Implement hash indexing in Table class
    Refactor Table class to use hash indexing and improve collision handling.

--- entities/tabela.py
+++ entities/tabela.py
@@ -1,12 +1,12 @@
 from entities.tupla import Tuple
 from entities.pagina import Page
-from utils.hash import Hasher
+
 
 class Table:
     def __init__(self, page_size) -> None:
         self.pages: list[Page] = [Page(size=page_size)]
         self.page_size = page_size
-        self.index = Index(page_size=self.page_size)
+        self.hash_index = {}
         
     def insert_page(self, page):
         self.pages.append(page)
@@ -30,10 +30,34 @@ class Table:
         else:
             raise ValueError("Deve fornecer key e value, ou uma tuple")
 
-    def generate_hashes(self, buckets):
-        for nth_page, page in enumerate(self.pages):
+    def generate_hashes(self, bucket_size):
+        n_collisions = 0
+        for nth_page, page in enumerate(table.get_pages()):
             for tuple in page.get_data():
-                hash = Hasher.hash(tuple.get_key())
+                hash = Hasher.hash(tupla.get_key())
+                if hash in hash_index.keys():
+                    n_collisions += 1
+                    print(f"{n_collisions}° Collision! Hash: {hash}")
+                    self.hash_index[hash].insert_value(nth_page)
+                else:
+                    self.hash_index[hash] = Bucket(bucket_size)
+                    self.hash_index[hash].insert_value(nth_page)
+
+    def get_pages(self):
+        return self.pages
+
+    def get_page(self, i):
+        return self.pages[i]
+
+    def __str__(self):
+        s = ""
+        for nth_page, page in enumerate(self.pages):
+            for i, tuple in enumerate(page.get_data()):
+                if i < len(page.get_data()):
+                    s += f"Page: {nth_page+1}: {tuple.get_key()},\n"
+                else:
+                    s += f"Page: {nth_page+1}: {tuple.get_key()}" 
+        return s
 
 class Index(Table):
     def __init__(self, page_size) -> None:
